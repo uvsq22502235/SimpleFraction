60938293e946ee18eaa70b9d1e98cb861e5980a4
uvsq22502235 <yanmohuang3@gmail.com>
2025-09-20 12:15:55 +0200

    Ajoutez les constructeurs et tester

--- src/main/java/com/example/TD1/Fraction.java
+++ src/main/java/com/example/TD1/Fraction.java
@@ -3,9 +3,24 @@ package com.example.TD1;
 public class Fraction {
     @Override
     public String toString() {
-        return "Je suis une fraction.";
+        return numerateur + "/" + denominateur;
     }
 
     private int numerateur;
     private int denominateur;
+
+    public Fraction(int numerateur, int denominateur) {
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+
+    public Fraction(int numerateur) {
+        this.numerateur = numerateur;
+        this.denominateur = 1;
+    }
+
+    public Fraction() {
+        this.numerateur = 0;
+        this.denominateur = 1;
+    }
 }
--- src/main/java/com/example/TD1/Main.java
+++ src/main/java/com/example/TD1/Main.java
@@ -2,7 +2,12 @@ package com.example.TD1;
 
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction();
-        System.out.println(f.toString());
+        Fraction f1 = new Fraction(3,4);
+        assert f1.toString().equals("3/4");
+        Fraction f2 = new Fraction(3);
+        assert f2.toString().equals("3/1");
+        Fraction f3 = new Fraction();
+        assert f3.toString().equals("0/1");
+        System.out.println("Tout les constructeurs marchent bien! ");
     }
 }
